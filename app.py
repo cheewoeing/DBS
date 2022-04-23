23df5eb79d21eed247d5a4a676497e916dbb21db
cheewoeing <104248593+cheewoeing@users.noreply.github.com>
2022-04-23 16:57:11 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[118]:
+
+
+from flask import Flask, render_template, request
+
+
+# In[119]:
+
+
+app = Flask(__name__)
+
+
+# In[120]:
+
+
+import joblib
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method =="POST":
+        rate = float(request.form.get("rate"))
+        print("rate")
+        print(rate)
+        model = joblib.load("DBS_Prediction")
+        pred = model.predict([[rate]])
+        return(render_template("index.html", result=pred))
+    else:
+        return(render_template("index.html", result="waiting"))
+        
+    
+
+
+# In[ ]:
+
+
+if __name__ =="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
